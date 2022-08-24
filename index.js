5a9da613a51518118355c7ef26c4fd3921aee857
jason mwaura mbugua <jason.mwaura@student.moringaschool.com>
2022-08-24 22:49:04 +0300

    assignment done

---
+++ index.js
@@ -0,0 +1,12 @@
+//Arrow functions Lab
+//jason
+
+
+const divide = (parameter1, parameter2) => parameter1/parameter2;
+divide(2000/100); //=> 20
+
+const nums = [2];
+const squares = nums.map(squares => x ** 2); 
+squares; //=> [4]
+nums; //=> [2]
+
